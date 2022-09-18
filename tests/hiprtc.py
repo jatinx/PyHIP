83bc95c226b57f6add9e3b79d9b0454894e8c8cf
Jatin Chaudhary <ndjatin@gmail.com>
2022-09-18 12:24:53 +0100

    Add execute to hiprtc test

--- tests/hiprtc.py
+++ tests/hiprtc.py
@@ -1,4 +1,5 @@
 from pyhip import hiprtc, hip
+import ctypes
 
 
 def test_hiprtcCompileProgram():
@@ -17,4 +18,15 @@ def test_hiprtcCompileProgram():
     assert module != None
     kernel = hip.hipModuleGetFunction(module, 'kernel')
     assert kernel != None
+    ptr = hip.hipMalloc(4)
+    assert ptr != None
+    
+    class PackageStruct(ctypes.Structure):
+        _fields_ = [("a", ctypes.c_void_p)]
+    struct = PackageStruct(ptr)
+    hip.hipModuleLaunchKernel(kernel, 1, 1, 1, 1, 1, 1, 0, 0, struct)
+    res = (ctypes.c_int * 1)()
+    hip.hipMemcpy_dtoh(ctypes.byref(res), ptr, 4)
+    assert res[0] == 10
+
     hiprtc.hiprtcDestroyProgram(prog)
